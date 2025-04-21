1aa0b772c3dafa836229c6ef648ed0235289cc58
Spyup <jason88tu@gmail.com>
2025-04-21 23:09:21 +0800

    fix: layout and footer sytle

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,10 +1,16 @@
 import type { Theme } from 'vitepress'
+import Banner from '#components/Banner.vue'
 import DefaultTheme from 'vitepress/theme'
+import { h } from 'vue'
 // https://vitepress.dev/guide/custom-theme
 import Layout from './Layout.vue'
 import './style.css'
 
 export default {
   extends: DefaultTheme,
-  Layout,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      'home-hero-before': () => h(Banner),
+    })
+  },
 } satisfies Theme
