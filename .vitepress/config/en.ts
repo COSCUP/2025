dbef178fc8718ee361e9172cb5ab046470cf572f
Spyup <jason88tu@gmail.com>
2025-04-24 19:57:12 +0800

    chore: add sidebar link

--- .vitepress/config/en.ts
+++ .vitepress/config/en.ts
@@ -8,9 +8,21 @@ export const en = defineConfig({
       { text: 'Event', link: '/en/event' },
       { text: 'About', link: '/en/about' },
     ],
-    sidebar: {},
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/COSCUP' },
-    ],
+    sidebar: {
+      '/participate/': [
+        {
+          text: 'Participate',
+          link: '/participate/attendee',
+          items: [
+            { text: 'As Attendee', link: '/participate/attendee' },
+            { text: 'As Community', link: '/participate/community' },
+            { text: 'As Speaker', link: '/participate/speaker' },
+            { text: 'As Sponsor', link: '/participate/sponsor' },
+            { text: 'As Organizing', link: '/participate/organizing' },
+          ],
+        },
+      ],
+    },
+    socialLinks: [{ icon: 'github', link: 'https://github.com/COSCUP' }],
   },
 })
--- .vitepress/config/zh_tw.ts
+++ .vitepress/config/zh_tw.ts
@@ -7,13 +7,25 @@ export const zh_tw = defineConfig({
     nav: [
       { text: '首頁', link: '/' },
       { text: '年會資訊', link: '/event' },
+      { text: 'Participate', link: '/participate' },
       { text: '關於我們', link: '/about' },
     ],
-    sidebar: {},
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/COSCUP' },
-    ],
-
+    sidebar: {
+      '/participate/': [
+        {
+          text: '參與指南',
+          link: '/participate/attendee',
+          items: [
+            { text: '身為「會眾」', link: '/participate/attendee' },
+            { text: '身為「社群」', link: '/participate/community' },
+            { text: '身為「講者」', link: '/participate/speaker' },
+            { text: '身為「贊助夥伴」', link: '/participate/sponsor' },
+            { text: '身為」籌備團隊」', link: '/participate/organizing' },
+          ],
+        },
+      ],
+    },
+    socialLinks: [{ icon: 'github', link: 'https://github.com/COSCUP' }],
     docFooter: {
       prev: '上一頁',
       next: '下一頁',
