0c0400bcbc6ed76f444cacefeef7d27970794752
Spyup <jason88tu@gmail.com>
2025-05-14 19:28:12 +0800

    fix: some error after rebase

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-import { useData, useRoute } from 'vitepress'
+import VPContent from '#components/VPContent.vue'
+import { inBrowser, useData, useRoute } from 'vitepress'
 import VPBackdrop from 'vitepress/dist/client/theme-default/components/VPBackdrop.vue'
 import VPLocalNav from 'vitepress/dist/client/theme-default/components/VPLocalNav.vue'
 import VPNav from 'vitepress/dist/client/theme-default/components/VPNav.vue'
 import VPSkipLink from 'vitepress/dist/client/theme-default/components/VPSkipLink.vue'
+
 import { useSidebar } from 'vitepress/dist/client/theme-default/composables/sidebar.js'
-
 import { computed, provide, useSlots, watch, watchEffect } from 'vue'
-import VPContent from '/@/components/VPContent.vue'
 
-const { frontmatter } = useData()
+const { lang, frontmatter } = useData()
 
 const {
   isOpen: isSidebarOpen,
